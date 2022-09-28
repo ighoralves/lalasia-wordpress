76fee504a554264fc42d29a8158014571dede43d
IghorAlves <ighoralc@gmail.com>
2022-09-28 11:59:56 -0300

    inicio

--- index.php
+++ index.php
@@ -112,22 +112,100 @@ get_header();
 				<div class="swiper mySwiper">
 					<div class="swiper-wrapper">
 						<div class="swiper-slide">
-							<picture>
-							<source media="(min-width: 361px)" srcset="<?php echo get_template_directory_uri() ?>/assets/img/">
-								<img src="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-1.png" alt="">
-							</picture>
+							<div class="Product-image">
+								<picture>
+									<source media="(min-width: 361px)" srcset="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-4.png">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-1.png" alt="">
+								</picture>
+
+							</div>
+							<div class="Product-discricao">
+								<p1 class="Product-text1">Chair</p1>
+								<h3 class="Product-text2">White Aesthetic Chair</h3>
+								<p class="Product-text3">Combination of wood and wool</p>
+								<strong>$63,47</strong>
+							</div>
 						</div>
 						<div class="swiper-slide">
-							<img src="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-2.png" alt="">
+							<div class="Product-image">
+								<picture>
+									<source media="(min-width: 361px)" srcset="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-5.png">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-2.png" alt="">
+								</picture>
+
+							</div>
+							<div class="Product-discricao">
+								<p1 class="Product-text1">Chair</p1>
+								<h3 class="Product-text2">White Aesthetic Chair</h3>
+								<p class="Product-text3">Combination of wood and wool</p>
+								<strong>$63,47</strong>
+							</div>
 						</div>
 						<div class="swiper-slide">
-							<img src="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-3.png" alt="">
+							<div class="Product-image">
+								<picture>
+									<source media="(min-width: 361px)" srcset="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-6.png">
+									<img src="<?php echo get_template_directory_uri() ?>/assets/img/Rectangle-3.png" alt="">
+								</picture>
+
+							</div>
+							<div class="Product-discricao">
+								<p1 class="Product-text1">Chair</p1>
+								<h3 class="Product-text2">White Aesthetic Chair</h3>
+								<p class="Product-text3">Combination of wood and wool</p>
+								<strong>$63,47</strong>
+							</div>
 						</div>
 					</div>
       		<div class="swiper-pagination"></div>
     		</div>
 			</div>
 		</section>
+
+		<section class="Craft">
+			<div class="Container">
+				<div class="Craft-content">
+					<h2 class="Title-mobile-left">Our Product</h2>
+					<h3 class="Text-mobile-left">Crafted by talented and high quality material</h3>
+					<p class="Paragraph-mobile-left">
+						Pellentesque etiam blandit in tincidunt at donec. 
+						Eget ipsum dignissim placerat nisi, adipiscing mauris 
+						non purus parturient.
+					</p>
+				</div>
+
+				<div class="Craft-btn">
+					<button class="Craft-btns" type="submit"><a href="#">Learn More</a></button>
+				</div>
+
+				<div class="Craft-numbers">
+					<div class="Craft-number">
+						<strong class="Craft-text1">20+</strong>
+						<p class="Craft-text2">Years Experience</p>
+					</div>
+					<div class="Craft-number">
+						<strong class="Craft-text1">483</strong>
+						<p class="Craft-text2">Happy Client</p>
+					</div>
+					<div class="Craft-number">
+						<strong class="Craft-text1">150+</strong>
+						<p class="Craft-text2">Project Finished</p>
+					</div>
+				</div>
+
+				<div class="Craft-tumb">
+					<picture class="Craft-image">
+						<source media="(min-width: 361px)" srcset="">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/img/img-3.png" alt="">
+					</picture>
+					<picture class="Craft-image">
+						<source media="(min-width: 361px)" srcset="">
+						<img src="<?php echo get_template_directory_uri() ?>/assets/img/img-4.png" alt="">
+					</picture>
+				</div>
+			</div>
+		</section>
+
 	</main>
 <?php
 get_footer();
