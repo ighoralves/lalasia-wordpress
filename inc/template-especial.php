f3bb6f54752826f3710a66fcc2bd6c967105d57b
IghorAlves <ighoralc@gmail.com>
2022-10-25 11:08:01 -0300

    inicio

---
+++ inc/template-especial.php
@@ -0,0 +1,11 @@
+<section class="Especial">
+			<div class="Container">
+				<div class="Especial-content">
+					<h3 class="Especial-text1">
+						Join with me for get special
+						discount
+					</h3>
+					<button class="Especial-btn" type="submit"><a href="#">Learn More</a></button>
+				</div>
+			</div>
+		</section>
--- index.php
+++ index.php
@@ -396,18 +396,8 @@ get_header();
 			</div>
 		</section>
 
-		<section class="Especial">
-			<div class="Container">
-				<div class="Especial-content">
-					<h3 class="Especial-text1">
-						Join with me for get special
-						discount
-					</h3>
-					<button class="Especial-btn" type="submit"><a href="#">Learn More</a></button>
-				</div>
-			</div>
-		</section>
-
+		<?php include(TEMPLATEPATH . '/inc/template-especial.php'); ?>
+		
 	</main>
 
 <?php
