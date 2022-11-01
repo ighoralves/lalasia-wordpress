b6e849a4b5affa62f675e5379938abceff383a15
IghorAlves <ighoralc@gmail.com>
2022-11-01 17:28:22 -0300

    inicio

--- footer.php
+++ footer.php
@@ -1,5 +1,64 @@
-	<footer>
-		footer
+	<footer class="Footer">
+		<div class="Container">
+			<div class="Linha"></div>
+
+			<main>
+				<div class="Footer-content">
+
+					<div class="Footer-card">
+						<figure class="Footer-logo">
+							<img src="<?php echo get_template_directory_uri() ?>/assets/img/frame.png" alt="">
+						</figure>
+						<h1 class="Footer-text"><a href="/projeto">lalasia</a></h1>
+					</div>
+
+					<div class="Footer-texts">
+						<p class="Paragraph-mobile-left">
+							Lalasia is digital agency that help you make better experience iaculis cras in.
+						</p>
+					</div>
+
+				</div>
+
+				<div class="Footer-menu">
+
+					<div class="Footer-menus">
+
+						<h3 class="Footer-text1">Product</h3>
+
+						<ul class="Footer-list">
+							<li><a href="">New Arrivals</a></li>
+							<li><a href="">Best Selling</a></li>
+							<li><a href="">Home Decor</a></li>
+							<li><a href="">Kitchen Set</a></li>
+						</ul>
+					</div>
+					<div class="Footer-menus">
+
+						<h3 class="Footer-text1">Services</h3>
+
+						<ul class="Footer-list">
+							<li><a href="">Catalog</a></li>
+							<li><a href="">Blog</a></li>
+							<li><a href="">Faq</a></li>
+							<li><a href="">Pricing</a></li>
+						</ul>
+					</div>
+					<div class="Footer-menus">
+
+						<h3 class="Footer-text1">Follow Us</h3>
+
+						<ul class="Footer-list">
+							<li><a href="">Facebook</a></li>
+							<li><a href="">Instagram</a></li>
+							<li><a href="">Twitter</a></li>
+						</ul>
+					</div>
+
+				</div>
+
+			</main>
+		</div>
 	</footer>
 </div>
 
